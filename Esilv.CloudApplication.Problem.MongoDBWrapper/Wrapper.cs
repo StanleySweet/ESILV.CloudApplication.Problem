f36e32c447f1c48b78efeede93b6cfcdc25d637e
Stanislas DOLCINI <stanislas.dolcini@devinci.fr>
2017-12-13 13:20:49 +0100

    Initial commit

---
+++ Esilv.CloudApplication.Problem.MongoDBWrapper/Wrapper.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Driver.Core.Connections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Esilv.CloudApplication.Problem.MongoDBWrapper
+{
+    public class Wrapper
+    {
+        private IConnection _connection;
+        private CancellationToken _cancellationToken;
+        private CancellationTokenSource _source;
+
+        public Wrapper()
+        {
+
+        }
+
+        public void Connect()
+        {
+            this._source = new CancellationTokenSource();
+            this._connection.Open(_source.Token);
+        }
+
+        public void Disconnect()
+        {
+            this._source.Cancel();
+        }
+
+    }
+}
