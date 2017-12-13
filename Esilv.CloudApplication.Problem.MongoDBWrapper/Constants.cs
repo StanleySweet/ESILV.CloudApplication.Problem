0b17c97a6b1b17aa1163a2853fb08f6e9eb126db
Stanislas DOLCINI <stanislas.dolcini@devinci.fr>
2017-12-13 09:43:58 +0100

    Ajoutez des fichiers projet.

---
+++ Esilv.CloudApplication.Problem.MongoDBWrapper/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esilv.CloudApplication.Problem.MongoDBWrapper
+{
+    public static class Constants
+    {
+        public static string CONNECTION_STRING = "";
+        public static string PORT = "";
+
+    
+    }
+}
