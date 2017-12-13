468326e3fd81d055c48581dd0fbfce13a789fcf5
Stanislas DOLCINI <stanislas.dolcini@devinci.fr>
2017-12-13 21:51:29 +0100

    Base skeleton architecture based on other works on github.

--- Esilv.CloudApplication.Problem.MongoDBWrapper/Wrapper.cs
+++ Esilv.CloudApplication.Problem.MongoDBWrapper/Wrapper.cs
@@ -1,4 +1,6 @@
-﻿using MongoDB.Driver.Core.Connections;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Connections;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,24 +12,49 @@ namespace ESILV.CloudApplication.Problem.MongoDBWrapper
 {
     public class Wrapper
     {
-        private IConnection _connection;
-        private CancellationToken _cancellationToken;
-        private CancellationTokenSource _source;
+        private MongoClient _client;
+        private IMongoDatabase _database;
 
         public Wrapper()
         {
-
+            _client = new MongoClient(Constants.CONNECTION_STRING);
         }
 
-        public void Connect()
+        public void Connect(string dbName)
         {
-            this._source = new CancellationTokenSource();
-            this._connection.Open(_source.Token);
+            _database = _client.GetDatabase(dbName);
+            var collection = _database.GetCollection<BsonDocument>("tokens");
+
+            Task t = queryDatabase(collection);
+            t.ContinueWith((str) =>
+            {
+                Console.WriteLine(str.Status.ToString());
+                Console.WriteLine("Query Ends.");
+            });
+            t.Wait();
+            Console.ReadKey();
         }
+
+        public async static Task<string> queryDatabase(IMongoCollection<BsonDocument> collection)
+        {
+            Console.WriteLine("Query Starts...");
+
+            IAggregateFluent<BsonDocument> aggregate = collection.Aggregate()
+                .Group(new BsonDocument { { "_id", "$token" }, { "count", new BsonDocument("$sum", 1) } })
+                .Sort(new BsonDocument { { "count", -1 } })
+                .Limit(10);
+            List<BsonDocument> results = await aggregate.ToListAsync();
+            foreach (BsonDocument obj in results)
+            {
+                Console.WriteLine(obj.ToString());
+            }
+            return "finished";
+        }
+
 
         public void Disconnect()
         {
-            this._source.Cancel();
+
         }
 
     }
